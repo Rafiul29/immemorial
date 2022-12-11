84dbc793d33478023e2020d592b91d2c39b547e9
Rafiul29 <rafiulislam4472@gmail.com>
2022-12-12 02:21:16 +0600

    hero section done

--- src/hooks/gsap.js
+++ src/hooks/gsap.js
@@ -3,6 +3,7 @@ import gsap,{Expo} from "gsap";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+
 export const useGsapShutterUnveil = (item,delay=0,trig) => {
   useEffect(() => {
     const el = item.current;
@@ -35,8 +36,45 @@ export const useGsapDownStagger=(arr,delay)=>{
       opacity:1,
       duration:1.5,
       stagger:0.1,
-      case:Expo.easeInOut,
+      case:Expo.easeIn,
       delay:delay
     })
     })
 }
+
+export const useGsapPhotoDroping=(arr)=>{
+  useEffect(()=>{
+const el=arr.map((item)=>item.current)
+    gsap.fromTo(el,{
+      y:"-100vh",
+      scale:0
+    },{
+      y:0,
+      scale:1,
+      duration:2,
+      stagger:0.2,
+      delay:2.7,
+      case:Expo.easeInOut,
+    })
+  },[])
+}
+export const useGsapPhotoLevitate = (arr, trig) => {
+  useEffect(() => {
+   const el=arr.map(item=>item.current)
+   gsap.fromTo(
+    el,
+    {
+      y: 0,
+    },
+    {
+      y: "-35%",
+      ease: Expo.easeInOut,
+      scrollTrigger: {
+        trigger: trig.current,
+        scrub: 1,
+        toggleActions: "play reverse play reverse",
+      },
+    }
+  );
+  }, []);
+};
