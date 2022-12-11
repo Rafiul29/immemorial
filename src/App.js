428b0f7d09c963870b0ce596345427e47c6509c4
Rafiul29 <rafiulislam4472@gmail.com>
2022-12-11 23:46:34 +0600

    immemorial nav and hero section done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes,Route } from "react-router-dom"
+import Home from "./components/Home"
+import Navbar from "./components/Navbar"
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Navbar/>
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+     </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+
+
+const Home = () => {
+  return (
+    <div>
+      <Hero/>
+    </div>
+  )
+}
+
+export default Home;
